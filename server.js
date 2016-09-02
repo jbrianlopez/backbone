ac6bcae2daff61566effaf2f9ebd3c52361d72f1
Brian Lopez <jbrianlopez@gmail.com>
2016-09-02 22:01:55 +0800

    added backend for heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
+
+
+app.use(express.static(__dirname))
+
+app.listen(port, function () {
+console.log('listening to' + port)
+})
+
+app.get('/', function (req, res) {
+  res.sendFile('.index.html')
+})
